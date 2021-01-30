819c74e5f0b1a880c3ae574a823773c294c3f5c2
Elliot Hershberg <eahershberg@gmail.com>
2021-01-30 09:03:28 -0800

    Reformat app example

--- app.R
+++ app.R
@@ -1,15 +1,12 @@
 library(shiny)
 library(RBrowse)
 
-ui <- fluidPage(
-  titlePanel("RBrowse Example"),
-  RBrowseOutput('widgetOutput')
-)
+ui <- fluidPage(titlePanel("RBrowse Example"),
+                RBrowseOutput('widgetOutput'))
 
 server <- function(input, output, session) {
-  output$widgetOutput <- renderRBrowse(
-    RBrowse("ViewHg19", location = "10:29,838,737..29,838,819")
-  )
+  output$widgetOutput <- renderRBrowse(RBrowse("ViewHg19",
+                                               location = "10:29,838,737..29,838,819"))
 }
 
 shinyApp(ui, server)
