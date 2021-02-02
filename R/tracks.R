15ba963f22fc2acc5e55bff98467490a7fa207b9
Elliot Hershberg <eahershberg@gmail.com>
2021-02-02 09:46:53 -0800

    Rename track to tracks, create working alignments track

---
+++ R/tracks.R
@@ -0,0 +1,94 @@
+#' Title
+#'
+#' @param type
+#' @param track_file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tracks <- function(...) {
+  track_values <- stringr::str_c(..., sep = ", ")
+  stringr::str_c("[", track_values, "]")
+}
+
+
+#' Title
+#'
+#' @param data
+#' @param assembly_name
+#'
+#' @return
+#' @export
+#'
+#' @examples
+track_alignments <- function(data, assembly_name) {
+  # print("It's an alignments track!")
+  type <- "AlignmentsTrack"
+  name <- get_name(data)
+  track_id <- stringr::str_c(assembly_name, "_", name)
+  adapter <- get_alignment_adapter(data)
+
+  # return the stringified JSON of track config
+  if (stringr::str_ends(data, ".bam")) {
+    as.character(
+      stringr::str_glue(
+        "{{",
+          '"type": "{type}", ',
+          '"name": "{name}", ',
+          '"assemblyNames": ["{assembly_name}"], ',
+          '"trackId": "{track_id}", ',
+          "{adapter} ",
+        "}}"
+      )
+    )
+  }
+}
+
+get_alignment_adapter <- function(data) {
+  if (stringr::str_ends(data, ".bam")) {
+    index <- stringr::str_c(data, ".bai")
+    as.character(
+      stringr::str_glue(
+        '"adapter": {{ ',
+          '"type": "BamAdapter", ',
+          '"bamLocation": {{ ',
+          '"uri": "{data}" ',
+          "}}, ",
+          '"index": {{ "location": {{ "uri": "{index}" }} }} ',
+        "}}"
+      )
+    )
+  } else if (stringr::str_ends(data, ".cram")) {
+    index <- stringr::str_c(data, ".crai")
+    as.character(
+      stringr::str_glue(
+        '"adapter": {{ ',
+          '"type": "CramAdapter", ',
+          '"cramLocation": {{ ',
+          '"uri": "{data}" ',
+          "}}, ",
+          '"craiLocation": {{ "uri": "{index}" }} ',
+        "}}"
+      )
+    )
+  } else {
+    stop("alignment data must be either BAM or CRAM")
+  }
+}
+
+# Interface ---------------------------------------------------------------
+
+# I actually probably want to expose a function per type of track.
+
+# The supported tracks are:
+
+# 'AlignmentsTrack'
+# 'PileupTrack'
+# 'SNPCoverageTrack'
+# 'VariantTrack'
+# 'WiggleTrack'
+
+# and then I want a tracks() function, that takes an arbitrary number
+# of tracks, and glues them into a string with [ at the beginning and ]
+# at the end, so that it will be the array of tracks in the config
