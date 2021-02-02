d4f9ed17654a738132b77cbd237bbbc3984a554e
Elliot Hershberg <eahershberg@gmail.com>
2021-02-02 14:32:18 -0800

    Add CRAM track to custom app

--- custom_app.R
+++ custom_app.R
@@ -9,8 +9,13 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   assembly <- assembly("https://jbrowse.org/genomes/hg19/fasta/hg19.fa.gz", bgzip = TRUE)
   tracks <- tracks(
-    track_alignments("https://s3.amazonaws.com/jbrowse.org/genomes/hg19/skbr3/reads_lr_skbr3.fa_ngmlr-0.2.3_mapped.down.bam",
-      assembly_name = "hg19"
+    track_alignments(
+      "https://s3.amazonaws.com/jbrowse.org/genomes/hg19/skbr3/reads_lr_skbr3.fa_ngmlr-0.2.3_mapped.down.bam",
+      assembly
+    ),
+    track_alignments(
+      "https://s3.amazonaws.com/jbrowse.org/genomes/hg19/ultra-long-ont_hs37d5_phased.cram",
+      assembly
     )
   )
 
