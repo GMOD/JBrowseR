bb35d6dd01699130bae3b95f1887ba017bacaa94
Elliot Hershberg <eahershberg@gmail.com>
2021-02-11 10:40:37 -0800

    Update assembly for aliases and refname aliases

--- R/assembly.R
+++ R/assembly.R
@@ -33,22 +33,28 @@
 #'
 #' @param assembly_data the URL or file path to your fasta file
 #' @param bgzip whether or not your fasta is bgzip compressed
+#' @param aliases a vector of strings of the aliases for the assembly
+#' @param refname_aliases the URL or file path to a file containing reference
+#'   name aliases. For more info see
+#'   \url{https://jbrowse.org/jb2/docs/config_guide#configuring-reference-name-aliasing}
 #'
 #' @return a string of RBrowse assembly configuration
 #' @export
 #'
 #' @examples
 #' assembly("https://jbrowse.org/genomes/hg19/fasta/hg19.fa.gz", bgzip = TRUE)
-assembly <- function(assembly_data, bgzip = FALSE) {
+assembly <- function(assembly_data, bgzip = FALSE, aliases = NULL, refname_aliases = NULL) {
   if (!bgzip) {
-    fa_assembly(assembly_data)
+    fa_assembly(assembly_data, aliases, refname_aliases)
   } else {
-    bgzip_fa_assembly(assembly_data)
+    bgzip_fa_assembly(assembly_data, aliases, refname_aliases)
   }
 }
 
-fa_assembly <- function(assembly_data) {
+fa_assembly <- function(assembly_data, aliases, refname_aliases) {
   name <- get_name(assembly_data)
+  aliases <- get_aliases(aliases)
+  refname_aliases <- get_refname_aliases(refname_aliases)
 
   # interpolate values into a string of JBrowse configuration
   #
@@ -57,6 +63,7 @@ fa_assembly <- function(assembly_data) {
     stringr::str_glue(
       "{{ ",
       '"name": "{name}", ',
+      "{aliases}",
       '"sequence": {{ ',
       '"type": "ReferenceSequenceTrack", ',
       '"trackId": "{name}-ReferenceSequenceTrack", ',
@@ -70,13 +77,16 @@ fa_assembly <- function(assembly_data) {
       "}} ",
       "}} ",
       "}} ",
+      "{refname_aliases}",
       "}}"
     )
   )
 }
 
-bgzip_fa_assembly <- function(assembly_data) {
+bgzip_fa_assembly <- function(assembly_data, aliases, refname_aliases) {
   name <- get_name(assembly_data)
+  aliases <- get_aliases(aliases)
+  refname_aliases <- get_refname_aliases(refname_aliases)
 
   # interpolate values into a string of JBrowse configuration
   #
@@ -85,6 +95,7 @@ bgzip_fa_assembly <- function(assembly_data) {
     stringr::str_glue(
       "{{ ",
       '"name": "{name}", ',
+      "{aliases}",
       '"sequence": {{ ',
       '"type": "ReferenceSequenceTrack", ',
       '"trackId": "{name}-ReferenceSequenceTrack", ',
@@ -101,18 +112,51 @@ bgzip_fa_assembly <- function(assembly_data) {
       "}} ",
       "}} ",
       "}} ",
+      "{refname_aliases}",
       "}}"
     )
   )
 }
 
-# Interface ---------------------------------------------------------------
-#
-# 1. For now, only indexedFasta or bigzipFasta
-#
-# 2. The name is always guessed from file name
-#
-# 3. No aliases for now
-#
-# 4.  If local file, must already be in same directory
-#     (or in directory in same place, i.e. data/hg19.fa)
+# create a JSON array of aliases for the config
+# c("hg19", "GRCh37") -> "aliases": ["hg19", "GRCh37"]
+get_aliases <- function(aliases) {
+  if (!is.null(aliases)) {
+    for (i in seq_along(aliases)) {
+      aliases[i] <- stringr::str_c('"', aliases[i], '"')
+    }
+
+    alias_array <- stringr::str_c(
+      "[",
+      stringr::str_c(aliases, collapse = ", "),
+      "]"
+    )
+
+    stringr::str_c(
+      '"aliases": ',
+      alias_array,
+      ", "
+    )
+  } else {
+    ""
+  }
+}
+
+get_refname_aliases <- function(refname_aliases) {
+  if (!is.null(refname_aliases)) {
+    as.character(
+      stringr::str_glue(
+        ', "refNameAliases": {{ ',
+        '"adapter": {{ ',
+        '"type": "RefNameAliasAdapter", ',
+        '"location": {{ ',
+        '"uri": "{refname_aliases}" ',
+        "}} ",
+        "}} ",
+        "}} "
+      )
+    )
+  } else {
+    ""
+  }
+}
