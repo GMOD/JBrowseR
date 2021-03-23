bf519ebfa6da12d327c07cb086d4827ffe39897c
Elliot Hershberg <eahershberg@gmail.com>
2021-03-23 12:11:45 -0700

    Style simple demo app

--- app.R
+++ app.R
@@ -1,12 +1,15 @@
 library(shiny)
 library(JBrowseR)
 
-ui <- fluidPage(titlePanel("JBrowseR Example"),
-                JBrowseROutput('widgetOutput'))
+ui <- fluidPage(
+  titlePanel("JBrowseR Example"),
+  JBrowseROutput("widgetOutput")
+)
 
 server <- function(input, output, session) {
   output$widgetOutput <- renderJBrowseR(JBrowseR("ViewHg19",
-                                               location = "10:29,838,737..29,838,819"))
+    location = "10:29,838,737..29,838,819"
+  ))
 }
 
 shinyApp(ui, server)
