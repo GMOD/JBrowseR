03ab30ef729d9acfccaf3c356bb8a3312810a58c
Colin <colin.diesh@gmail.com>
2023-08-30 10:58:27 -0400

    Plugins param

--- srcjs/components/JsonView.jsx
+++ srcjs/components/JsonView.jsx
@@ -7,17 +7,39 @@ import { messageShiny } from '../utils'
 
 export default function View(props) {
   const configObject = JSON.parse(props.config)
-  const { assembly, tracks, defaultSession, theme, text_index } = configObject
-  const location = props.location
-  const state = createViewState({
+  const {
     assembly,
     tracks,
     defaultSession,
-    location,
-    onChange: messageShiny,
-    configuration: { theme },
-    aggregateTextSearchAdapters: [text_index],
-  })
+    theme,
+    text_index,
+    plugins: pluginInput = [],
+  } = configObject
+  const location = props.location
 
-  return <JBrowseLinearGenomeView viewState={state} />
+  const [viewState, setViewState] = useState()
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    ;(async () => {
+      try {
+        const plugins = await loadPlugins(pluginInput)
+        const state = createViewState({
+          assembly,
+          tracks,
+          defaultSession,
+          location,
+          onChange: messageShiny,
+          configuration: { theme },
+          aggregateTextSearchAdapters: [text_index],
+          plugins: plugins.map(p => p.plugin),
+        })
+        state.session.view.showTrack('segdups_ucsc_hg19')
+        setViewState(state)
+      } catch (e) {
+        setError(e)
+      }
+    })()
+  }, [])
+
+  return <JBrowseLinearGenomeView viewState={viewState} />
 }
